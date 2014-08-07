6be509d4f1bdacd8f953e3aaa8a3c9e7ea733d4d
WilliamG <williamg@activestate.com>
2014-08-07 11:41:22 -0700

    Add jpda debug command, with hardcoded port for now

--- lib/java_buildpack/container/tomcat.rb
+++ lib/java_buildpack/container/tomcat.rb
@@ -38,7 +38,9 @@ module JavaBuildpack
         [
           @droplet.java_home.as_env_var,
           @droplet.java_opts.as_env_var,
+          'JPDA_ADDRESS=4000',
           "$PWD/#{(@droplet.sandbox + 'bin/catalina.sh').relative_path_from(@droplet.root)}",
+          'jpda',
           'run'
         ].flatten.compact.join(' ')
       end
